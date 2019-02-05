ba951bec01a08e6ab43f382e7969696d0221c98e
David.T.Nguyen <david.t.nguyen@target.com>
2019-02-04 21:21:46 -0600

    input form spec

--- cypress/integration/02-adding-items/input-form.spec.js
+++ cypress/integration/02-adding-items/input-form.spec.js
@@ -1,19 +1,22 @@
 describe('Input form', () => {
+  // 🥇 - FIND OUT WHAT THIS COMMAND DOES 🥇
   beforeEach(() => {
     cy.seedAndVisit([])
   })
 
+  // ❓ HOW TO SET FOCUS ON AN ELEMENT ❓
   it('focuses input on load', () => {
-    cy.focused()
-      .should('have.class', 'new-todo')
+    // 💁 https://docs.cypress.io/api/commands/focus.html#Usage
+    expect(false).to.be.true
   })
 
+  // ❓ HOW CAN I MAKE CYPRESS ENTER A VALUE INTO A TEXTBOX ❓
   it('accepts input', () => {
-    const typedText = 'Buy Milk'
-
-    cy.get('.new-todo')
-      .type(typedText)
-      .should('have.value', typedText)
+    // const typedText = 'Buy Milk'
+    // ❓ WHAT IS THE SELECTOR TO GET A HOLD OF THE TEXTBOX ❓
+    // ❓ WHAT IS THE COMMAND TO ACHIVE THE OBJECT
+    // 💁 https://docs.cypress.io/api/commands/type.html#Syntax
+    expect(false).to.be.true
   })
 
   context('Form submission', () => {
@@ -21,40 +24,16 @@ describe('Input form', () => {
       cy.server()
     })
 
+    // ❓ WHAT is the JSON Payload for ADDing a new item ❓
+    // 
     it('Adds a new todo on submit', () => {
-      const itemText = 'Buy eggs'
-      cy.route('POST', '/api/todos', {
-        name: itemText,
-        id: 1,
-        isComplete: false
-      })
-
-      cy.get('.new-todo')
-        .type(itemText)
-        .type('{enter}')
-        .should('have.value', '')
-
-      cy.get('.todo-list li')
-        .should('have.length', 1)
-        .and('contain', itemText)
+      expect(false).to.be.true
     })
 
+    // ❓ HOW TO MOCK A 500 ERROR ❓
+    // ❓ WHAT DOES THE UI LOOK AND BEHAVE ❓
     it('Shows an error message on a failed submission', () => {
-      cy.route({
-        url: '/api/todos',
-        method: 'POST',
-        status: 500,
-        response: {}
-      })
-
-      cy.get('.new-todo')
-        .type('test{enter}')
-
-      cy.get('.todo-list li')
-        .should('not.exist')
-
-      cy.get('.error')
-        .should('be.visible')
+      expect(false).to.be.true
     })
   })
 })
